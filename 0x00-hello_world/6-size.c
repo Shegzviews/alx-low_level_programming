4ceab2540b333164bb3cd8e65ca59cbeb0a2d442
Shegzviews <agunmaromitchell@gmail.com>
2022-09-08 16:43:20 -0700

    adding files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+* main - Prints the sizeof many var types
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle 
-* Return: 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("size of a char: %1d byte(s)\n", sizeof(c));
-	printf("size of an int: %1d byte(s)\n", sizeof(i));
-	printf("size of a long int: %1d byte(s)\n", sizeof(li));
-	printf("size of a long long int: %1d byte(s)\n", sizeof(lli));
-	printf("size of a float: %1d byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
